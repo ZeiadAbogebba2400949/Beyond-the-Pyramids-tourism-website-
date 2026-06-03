c71aaca44f744adda79a584d2ad2b845f43cf9b3
ZeiadAbogebba2400949 <zeiad2400949@miuegypt.edu.eg>
2026-06-03 20:33:55 +0300

    auth and user routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,12 +4,12 @@ const adminController = require('../controllers/adminController');
 const bookingController = require('../controllers/bookingController');
 const { protect, authorize } = require('../middleware/auth');
 
-router.get('/stats', protect, authorize('Admin'), adminController.getStats);
+router.get('/stats',    protect, authorize('Admin'), adminController.getStats);
 router.get('/activity', protect, authorize('Admin'), adminController.getRecentActivity);
 
 router.get('/users', protect, authorize('Admin'), adminController.getAdminUsers);
 
-router.get('/bookings', protect, authorize('Admin'), bookingController.getAllBookings);
+router.get('/bookings',          protect, authorize('Admin'), bookingController.getAllBookings);
 router.patch('/bookings/:id/status', protect, authorize('Admin'), bookingController.updateBookingStatus);
 
 module.exports = router;
